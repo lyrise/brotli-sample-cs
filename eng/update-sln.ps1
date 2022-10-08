e6c970ef4dd34d0463e7a684805f62388c4db712
Lyrise <lyrise1984@gmail.com>
2022-10-08 17:24:51 +0900

    fix shrink

--- eng/update-sln.ps1
+++ eng/update-sln.ps1
@@ -1,3 +1,2 @@
 dotnet new sln --force -n sample
 dotnet sln sample.sln add (ls -r ./src/**/*.csproj)
-dotnet sln sample.sln add (ls -r ./test/**/*.csproj)
--- eng/update-sln.sh
+++ eng/update-sln.sh
@@ -3,4 +3,3 @@ set -euo pipefail
 
 dotnet new sln --force -n sample
 dotnet sln sample.sln add ./src/**/*.csproj
-dotnet sln sample.sln add ./test/**/*.csproj
